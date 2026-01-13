a957e3a1c4e575022cc0e62c9054409f062236da
Nicole Chan <nictscnotes@gmail.com>
2026-01-13 19:47:04 +1100

    wrote our first django model

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models #plucking out models from the django 
 
-# Create your models here.
+
+# Create your models / database tables here.
+class Fundraiser(models.Model): # inheriting the Django Model from models and capturing in the Fundraiser class
+    title = models.CharField(max_length=200) # creating a class attribute named title and this becomes a column in the database table
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField(auto_now_add=True)
