9bf18d5b2c0cc554bb952c96df1297d41d122eab
Nicole Chan <nictscnotes@gmail.com>
2026-01-17 12:36:03 +1100

    create the first GET request

---
+++ crowdfunding/fundraisers/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path('fundraisers/', views.FundraiserList.as_view()),
+ ]
--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+# pulling 4 dependencies
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Fundraiser # from this models.py file, import this class called Fundraiser
+from .serializers import FundraiserSerializer # from the serializer file, import this class called FundraiserSerializer
+
+class FundraiserList(APIView):
+
+   def get(self, request):
+       fundraisers = Fundraiser.objects.all()
+       serializer = FundraiserSerializer(fundraisers, many=True)
+       return Response(serializer.data)
