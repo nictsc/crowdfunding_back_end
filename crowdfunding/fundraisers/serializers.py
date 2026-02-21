503559a37af63327912dde0bda2bde6ece05ce1f
Nicole Chan <nictscnotes@gmail.com>
2026-02-21 12:29:30 +1100

    Delete the delete function in FundraiserDetailSerializer

--- crowdfunding/fundraisers/serializers.py
+++ crowdfunding/fundraisers/serializers.py
@@ -30,13 +30,11 @@ class FundraiserDetailSerializer(FundraiserSerializer):
         instance.title = validated_data.get('title', instance.title)
         instance.description = validated_data.get('description', instance.description)
         instance.goal = validated_data.get('goal', instance.goal)
-        instance.image = validated_data.get('image', instance.goal)
+        instance.image = validated_data.get('image', instance.image)
         instance.is_open = validated_data.get('is_open', instance.is_open)
         instance.date_created = validated_data.get('date_created', instance.date_created)
         instance.owner = validated_data.get('owner', instance.owner)
         instance.save()
         return instance
     
-    def delete(self, instance, validated_data):
-        instance.is_deleted = validated_data.get('is_deleted', instance.is_deleted)
-
+    
